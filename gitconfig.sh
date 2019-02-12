7e98e45118d395dced9a7ddaad699f8cae1916f0
Oscar Olazabal <oscarolazabal@gmail.com>
2019-02-12 14:28:39 -0800

    update configuration for new branch structure

--- gitconfig.sh
+++ gitconfig.sh
@@ -6,11 +6,11 @@ git stash
 git remote add omolazabal https://github.com/omolazabal/si-spring-2019.git
 git fetch omolazabal
 
-git branch solutions omolazabal/master
+git branch solutions omolazabal/solutions
 git checkout solutions
 
 git fetch omolazabal
-git reset --hard omolazabal/master
+git reset --hard omolazabal/solutions
 
 git checkout master
 git stash apply
